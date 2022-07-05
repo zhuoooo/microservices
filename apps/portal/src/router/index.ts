85a0302f75b68ed1146bc84a7963dfed09ffc41a
Zhuoooo <16620154495@163.com>
2022-07-05 16:48:34 +0800

    chore: 初始化项目

--- apps/portal/src/router/index.ts
+++ apps/portal/src/router/index.ts
@@ -13,9 +13,6 @@ const routes: Array<RouteConfig> = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
---
+++ apps/portal/vue.config.js
@@ -0,0 +1,46 @@
+const { defineConfig } = require('@vue/cli-service')
+const fs = require('fs')
+const path = require('path')
+const ESLintPlugin = require('eslint-webpack-plugin')
+
+const isProd = ['production', 'prod'].includes(process.env.NODE_ENV);
+
+const appPackage = require('./package.json')
+const appName = appPackage.name
+
+function resolve(dir) {
+  return path.resolve(__dirname, dir);
+}
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    historyApiFallback: true,
+    allowedHosts: 'all',
+    port: 10000,
+    https: {
+      key: fs.readFileSync(resolve('../../certificate/cert.key')),
+      cert: fs.readFileSync(resolve('../../certificate/cert.crt')),
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+  },
+  configureWebpack: config => {
+    if (!isProd) {
+      config.plugins.push(new ESLintPlugin({
+        fix: true,
+        extensions: ['.jsx', '.js', '.vue', '.ts', '.tsx'],
+        threads: true,
+        lintDirtyModulesOnly: true
+      }));
+    }
+  },
+  chainWebpack: config => {
+    config.output.set('uniqueName', appName)
+    
+    config.output
+      .filename(`static/js/${appName}.[name].[chunkhash:8].js`)
+      .chunkFilename(`static/js/${appName}.[name].[chunkhash:8].js`)
+  }
+})
